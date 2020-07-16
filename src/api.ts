23f041187f5da25cc2530007801274187d824893
karimkhattaby <18755153+karimkhattaby@users.noreply.github.com>
2020-07-16 09:37:16 +0400

    Added GET Root

--- src/api.ts
+++ src/api.ts
@@ -4,5 +4,10 @@ import { Router } from "./deps.ts";
 // Initializing API Calls Router
 const router = new Router();
 
+// GET root
+router.get("/", (ctx) => {
+    ctx.response.body = "Welcome to Smart Brain API.";
+});
+
 // Exporting the Router
 export default router;
