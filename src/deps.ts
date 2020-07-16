4da457fd9749df1049a4a4b92a26b1ed5b115875
karimkhattaby <18755153+karimkhattaby@users.noreply.github.com>
2020-07-16 10:04:14 +0400

    Removed Unnecessary Dependency

--- src/deps.ts
+++ src/deps.ts
@@ -2,4 +2,4 @@
 export * as log from "https://deno.land/std/log/mod.ts";
 
 // Third Part Dependencies
-export { Application, send, Router } from "https://deno.land/x/oak@v5.0.0/mod.ts";
+export { Application, Router } from "https://deno.land/x/oak@v5.0.0/mod.ts";
