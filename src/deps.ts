5801797359502069a93a7d436e6149deb5656aff
karimkhattaby <18755153+karimkhattaby@users.noreply.github.com>
2020-07-16 11:06:44 +0400

    Added Postgres Client to Dependencies

--- src/deps.ts
+++ src/deps.ts
@@ -3,3 +3,4 @@ export * as log from "https://deno.land/std/log/mod.ts";
 
 // Third Part Dependencies
 export { Application, Router } from "https://deno.land/x/oak@v5.0.0/mod.ts";
+export { Client } from "https://deno.land/x/postgres/mod.ts";
