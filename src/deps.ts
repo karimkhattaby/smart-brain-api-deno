8ed1bf741177e3228cd77213a9555e1d9bc85ffd
karimkhattaby <18755153+karimkhattaby@users.noreply.github.com>
2020-07-16 09:35:31 +0400

    Added Logger and Oak to Dependencies

---
+++ src/deps.ts
@@ -0,0 +1,5 @@
+// Standard Library Dependencies
+export * as log from "https://deno.land/std/log/mod.ts";
+
+// Third Part Dependencies
+export { Application, send, Router } from "https://deno.land/x/oak@v5.0.0/mod.ts";
