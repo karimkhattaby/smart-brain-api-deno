19b9523f1052109f20f08a2a788be7d4b578d5f1
karimkhattaby <18755153+karimkhattaby@users.noreply.github.com>
2020-07-16 14:38:01 +0400

    Added POST SignIn

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,9 @@
 // Importing Core Dependencies
 import { Router, Client } from "./deps.ts";
 
+// Importing Controllers
+import * as signin from "./controllers/signin.ts";
+
 // Importing Database Settings
 import db_settings from "../private/db.config.ts"
 
@@ -14,6 +17,27 @@ await db.connect();
 // GET root
 router.get("/", (ctx) => {
     ctx.response.body = "Welcome to Smart Brain API.";
+});
+
+// POST SignIn
+router.post("/signin", async (ctx) => {
+    // Extract Body
+    const body = await ctx.request.body();
+
+    // Check for Bad Requests
+    if ( !body.value.email || !body.value.password ) {
+        ctx.response.body = "Bad Request";
+        ctx.response.status = 400;
+    } else {
+        const result = await signin.handleSignin(body.value, db);
+        ctx.response.body = result;
+        // Check for Errors
+        if (result === "Wrong Credentials" || result === "Unable to Get User") {
+            ctx.response.status = 400;
+        } else {
+            ctx.response.status = 201;
+        }
+    }
 });
 
 // Exporting the Router
