182628b024de15e233a5432b1623e9bbe7ee6d84
karimkhattaby <18755153+karimkhattaby@users.noreply.github.com>
2020-07-16 11:23:19 +0400

    Added oakCors to Dependencies

--- src/deps.ts
+++ src/deps.ts
@@ -4,3 +4,4 @@ export * as log from "https://deno.land/std/log/mod.ts";
 // Third Part Dependencies
 export { Application, Router } from "https://deno.land/x/oak@v5.0.0/mod.ts";
 export { Client } from "https://deno.land/x/postgres/mod.ts";
+export { oakCors } from "https://deno.land/x/cors/mod.ts";
