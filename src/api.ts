9c68bcfd940b90a090e9af763548ed5c600462cc
karimkhattaby <18755153+karimkhattaby@users.noreply.github.com>
2020-07-16 09:36:05 +0400

    Initialized Oak Router

---
+++ src/api.ts
@@ -0,0 +1,8 @@
+// Importing Core Dependencies
+import { Router } from "./deps.ts";
+
+// Initializing API Calls Router
+const router = new Router();
+
+// Exporting the Router
+export default router;
