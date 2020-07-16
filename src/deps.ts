b700df3f2f2c05103cdeea0255d90e1db0d72ca5
karimkhattaby <18755153+karimkhattaby@users.noreply.github.com>
2020-07-16 11:35:42 +0400

    Added bcrypt to Dependencies & Added Versioning to Third-Party Dependencies

--- src/deps.ts
+++ src/deps.ts
@@ -3,5 +3,6 @@ export * as log from "https://deno.land/std/log/mod.ts";
 
 // Third Part Dependencies
 export { Application, Router } from "https://deno.land/x/oak@v5.0.0/mod.ts";
-export { Client } from "https://deno.land/x/postgres/mod.ts";
+export { Client } from "https://deno.land/x/postgres@v0.4.2/mod.ts";
+export * as bcrypt from "https://deno.land/x/bcrypt@v0.2.3/mod.ts";
 export { oakCors } from "https://deno.land/x/cors/mod.ts";
